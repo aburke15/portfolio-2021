da636a171339fdfeeb66d2eb50d35c16f7273080
Andre Burke <aburke.dev@gmail.com>
2021-02-08 22:27:34 -0700

    added title text

--- pages/components/title.tsx
+++ pages/components/title.tsx
@@ -2,6 +2,11 @@ import styles from '../../styles/Title.module.css'
 
 export default function Title() {
     return (
-        <div className={styles.title}></div>
+        <div className={styles.titleImage}>
+            <div className={styles.titleText}>
+                <h1>André Burke</h1>
+                <h2>Software Engineer</h2>
+            </div>
+        </div>
     )
 }
